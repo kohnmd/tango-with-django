5cd0c36362d7558d461f253df04ab9adad9eab93
Mike Kohn <kohnmd@gmail.com>
2014-11-02 00:33:04 -0600

    HTML templates, static file serving

--- rango/views.py
+++ rango/views.py
@@ -2,13 +2,18 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    response = 'Rango says hey there world!';
-    response += '<br>';
-    response += '<a href="about">See what Rango is about.</a>';
-    return HttpResponse(response)
+    # Construct a dictionary to pass to the template engine as its context.
+    # Note the key boldmessage is the same as {{ boldmessage }} in the template!
+    context_dict = {'boldmessage': "I am bold font from the context"}
+
+    # Return a rendered response to send to the client.
+    # We make use of the shortcut function to make our lives easier.
+    # Note that the first parameter is the template we wish to use.
+    return render(request, 'rango/index.html', context_dict)
 
 def about(request):
-    response = 'Rango says here is the about page.';
-    response += '<br>';
-    response += '<a href="./">Back to index.</a>';
+    response = 'Rango says here is the about page.'
+    response += '<br>'
+    response += '<a href="./">Back to index.</a>'
+    response += '<br>'
     return HttpResponse(response)
--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
+TEMPLATE_PATH = os.path.join(BASE_DIR, 'templates')
+STATIC_PATH = os.path.join(BASE_DIR,'static')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media') # Absolute path to the media directory
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
@@ -53,6 +56,9 @@ ROOT_URLCONF = 'tango_with_django_project.urls'
 
 WSGI_APPLICATION = 'tango_with_django_project.wsgi.application'
 
+TEMPLATE_DIRS = [
+    TEMPLATE_PATH,
+]
 
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
@@ -81,4 +87,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
+MEDIA_URL = '/media/'
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    STATIC_PATH,
+)
